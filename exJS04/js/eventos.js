bb076479bc8737e8d45c673774ca11a24d30b691
APBielzinx <112442554+APBielzinx@users.noreply.github.com>
2023-03-28 10:34:56 -0300

    ex04 finalizado

--- exJS04/js/eventos.js
+++ exJS04/js/eventos.js
@@ -5,7 +5,7 @@ alert("Essa é a função Onclick");
 
 }
 
-
+//evento dblClick
 function funcaodblClick(){
 
 
@@ -14,15 +14,18 @@ function funcaodblClick(){
 
 }
 
+//evento dblClick
+
 function funcaoonChange() {
     var select = document.getElementById("select").value;
-    document.getElementById("demo").innerHTML = "voce selecionou o professor: " + select;
+    document.getElementById("demo").innerHTML = "você selecionou o professor: " + select;
   }
 
 
+//evento onLoad
 
-  function checkCookies()
-{
+  function funcaoonLoad(){
+
 if (navigator.cookieEnabled==true)
 	{
 	alert("Esse é o evento onload e os Cookies são permitidos")
@@ -32,31 +35,37 @@ else
 	alert("Esse é o evento onload e os Cookies Cookies não são permitidos")
 	}
 }
+//evento onMouseOver
 
-function mOver(obj)
-{
-obj.innerHTML="Obrigado"
+function funcaoonMouseOver(obj){
+  obj.innerHTML="Obrigado"
 }
 
-function mOut(obj)
-{
+//evento onMouseOut
+
+
+function funcaoonMouseOut(obj){
+
 obj.innerHTML="Passe o mouse em mim"
+
 }
 
+//evento onMouseDown
 
-function mDown(obj)
-{
-obj.style.backgroundColor="#1ec5e5";
+
+function funcaoonKeyDown(obj){
+obj.style.backgroundColor="#FFFF00";
 obj.innerHTML="Solte o clique"
 }
 
-function mUp(obj)
-{
-obj.style.backgroundColor="#D94A38";
+//evento onMouseUp
+
+function funcaoonKeyUp(obj){
+obj.style.backgroundColor="#0088ff";
 obj.innerHTML="Obrigado"
 }
 
 
-function myFunction(x) {
+function funcaoonFocus(x) {
     x.style.background = "yellow";
   }
