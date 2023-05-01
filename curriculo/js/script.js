c943410c6909dfbb1b0d66c2d7004e5920606338
APBielzinx <112442554+APBielzinx@users.noreply.github.com>
2023-05-01 13:27:55 -0300

    cabeçalho

--- curriculo/js/script.js
+++ curriculo/js/script.js
@@ -5,7 +5,7 @@ let tags = ['h1',
 'div',
 'h2',
 'h3',
-'i']
+'span']
 
 let listaTags = []
 
@@ -31,12 +31,29 @@ listaTags[5].textContent = "Emprego"
 
 //telefone
 listaTags[4].appendChild(listaTags[7])
-listaTags[7].classList.add('material-icons')
+listaTags[7].classList.add('material-symbols-outlined')
 listaTags[7].textContent = "call"
 listaTags[4].appendChild(listaTags[6])
 listaTags[6].textContent = "1213123"
 
+//emaill
+let iconEmail  = new criarTags('span')
+listaTags[4].appendChild(iconEmail)
+iconEmail.classList.add('material-symbols-outlined')
+iconEmail.textContent = "mail"
+let txtEmail = new criarTags('h3')
+listaTags[4].appendChild(txtEmail)
+txtEmail.textContent = "exemple@exemple.com"
 
+
+//rede social
+let iconRedeSocial = new criarTags('span')
+listaTags[4].appendChild(iconRedeSocial)
+iconRedeSocial.classList.add('material-symbols-outlined')
+iconRedeSocial.textContent = "language"
+let txtredeSocial = new criarTags('h3')
+listaTags[4].appendChild(txtredeSocial)
+txtredeSocial.textContent = "@example"
 
 
 function criarTags(tag){
